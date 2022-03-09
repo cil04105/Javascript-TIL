25c682525ac6d00d48cceba281b66b6aaf2d37ca
cil04105 <hojin6417@gmail.com>
2022-03-10 00:31:50 +0900

    Modify

--- spread.js
+++ spread.js
@@ -1,27 +1,28 @@
-// //Spread Operator
-// let arr = ["hello", "hi"];
-// console.log(...arr); //hello hi
+//Spread Operator
+let arr = ["hello", "hi"];
+console.log(...arr); //hello hi
 
-// let string = "hello world";
-// console.log(...string); //'h' 'e' 'l' 'l' 'o' 'w' 'o' 'r' 'l' 'd'
+let string = "hello world";
+console.log(...string); //'h' 'e' 'l' 'l' 'o' 'w' 'o' 'r' 'l' 'd'
 
-// //배열을 합치거나 복사할때 사용
-// let a = [1, 2, 3];
-// let b = [4, 5];
+//배열을 합치거나 복사할때 사용
+let a = [1, 2, 3];
+let b = [4, 5];
 
-// let c = [...a, ...b];
-// console.log(c); // [1, 2, 3, 4, 5]
+let c = [...a, ...b];
+console.log(c); // [1, 2, 3, 4, 5]
 
-// //배열 복사하기(deep copy)
-// let a = [1, 2, 3];
-// let b = a; // 그냥 복사하면 값을 공유함
-// let c = [...a]; // spread 연신자 사용하면 독릭적인 값을 가진다.
+//배열 복사하기(deep copy)
+let a = [1, 2, 3];
+let b = a; // 그냥 복사하면 값을 공유함
+let c = [...a]; // spread 연신자 사용하면 독릭적인 값을 가진다.
 
-// //오브젝트도 사용 가능
-// let obj1 = { a: 1, b: 2 };
-// let obj2 = { ...obj1, c: 3, d: 4 };
-// let obj3 = { ...obj1, a: 3, b: 4 };
-// console.log(obj3); // 값 중복 시 뒤에 있는 값으로 적용됨
+//오브젝트도 사용 가능
+let obj1 = { a: 1, b: 2 };
+let obj2 = { ...obj1, c: 3, d: 4 };
+
+let obj3 = { ...obj1, a: 3, b: 4 };
+console.log(obj3); // 값 중복 시 뒤에 있는 값으로 적용됨
 
 //함수 안에 파라미터 넣을 때 사용
 let arr = [1, 2, 3];
---
+++ var-let-const.js
@@ -0,0 +1,18 @@
+//변수? 자료를 임시로 저장하는 공간(선언, 할당, 범위)
+
+//재선언, 재할당 가능 / function 안에서만 사용 가능
+var name = "Lee";
+var name = "Park"; //재할당 가능(값 변경)
+
+function 함수() {
+  var name = "Kim";
+}
+
+//let, const 는 거의 모든 중괄호 안에서 사용 가능
+let name = "Lee";
+const name = "Lee"; // 재할당 불가능(상수)
+
+/*정리*/
+// var 는 재선언 재할당이 가능하다.
+// let 과 const 는 재선언이 불가능하고 중복 시 에러가 발생한다.
+// let 은 재할당이 가능하지만 const는 상수이기 때문에 재할당이 불가능하다.
