b08f3329bcc61e56fccd25258d235b0e60a6ab22
cil04105 <hojin6417@gmail.com>
2022-03-13 23:59:11 +0900

    Add : async

--- promis.js
+++ promis.js
@@ -2,36 +2,42 @@
 // 성공, 실패를 판정해주는 문법
 // 프로미스가 성공일 경우 실행
 
-let prm = new Promise(function (resolve, rejecte) {
-  let data = "Hello world";
-  resolve(data);
-});
+// let prm = new Promise(function (resolve, rejecte) {
+//   let data = "Hello world";
+//   resolve(data);
+// });
 
-prm
-  .then((data) => {
-    console.log(data);
-  })
-  .catch(function () {
-    console.log("실패");
-  });
+// prm
+//   .then((data) => {
+//     console.log(data);
+//   })
+//   .catch(function () {
+//     console.log("실패");
+//   });
 
 //5초 후 실행되는 코드
 //콘솔창에 prm 입력하면 상태 확인 가능
 let prm = new Promise((resolve, reject) => {
   setTimeout(function () {
+    console.log(1);
     let data = "Hellow World"; //pending
     resolve(data); //resolved
-  }, 5000);
+    console.log(2);
+  }, 5000); // 비동기처리 x 그냥 5초 동안 브라우저 멈춤
 });
 
 prm
   .then((data) => {
-    console.log(data);
+    console.log(`${data}`);
   })
   .catch(() => {
     console.log("실패");
   });
 
 // 정리
+//자바스크립트에서 비동기적 처리를 간편하게 처리할 수 있도록 도와주는 오브젝트
+//상태
+
+//순차적 실행을 위해 콜백함수 대신 쓸 수 있는 코딩 패턴
 //콜백 함수 디자인 패턴이 복잡해서 생겨난 디자인 패턴(동기적 처리를 비동기 처리로 바꾸는게 아니다)
 //그냥 콜백함수 디자인 중에 하나임
---
+++ test.js
@@ -0,0 +1,25 @@
+// console.log(getNum(10));
+// function getNum(number) {
+//   return number;
+// }
+
+// console.log(test(15));
+// let test = function (a) {
+//   return a;
+// };
+
+console.log(myName);
+let myName;
+myName = "hojin";
+
+// apply : call 함수와 유사하지만, 매개변수는 배열로 받는것에 있어 차이가 있다.
+// call : 객채를 바인딩 함과 동시에 호출을 한다.
+// bind : 바인딩이된 함수를 반환하며, 한번더 호출시 함수를 실행한다.
+
+// 1. 그냥 일반 함수에서 쓰면 window
+
+// 2. 오브젝트 내의 함수(메소드)에서 쓰면 함수를 동작시키는 오브젝트
+
+// 가 출력된다고 배워봤습니다.
+
+//먼저 기명 함수 앞에 어싱크 키워드를 붙이고 함수 내부에서 동기처리 처럼 작동할 비동기 작업들에 어웨이트를 붙여 작성합니다
